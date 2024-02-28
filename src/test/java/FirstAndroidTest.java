b37107f6dc944b6862212db74922e643cdcc100a
NguyenDuyThanhB1909984 <101155685+NguyenDuyThanhB1909984@users.noreply.github.com>
2024-02-28 09:25:01 +0700

    Update FirstAndroidTest.java

--- src/test/java/FirstAndroidTest.java
+++ src/test/java/FirstAndroidTest.java
@@ -1,32 +1,35 @@
+
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-
 public class FirstAndroidTest {
-
     AppiumDriver driver;
 
     @BeforeTest
     public void setUp() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "12.0");
+//        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("platformVersion", "14.0");
         caps.setCapability("deviceName", "emulator-5554");
-        caps.setCapability("forceEspressoRebuild",true);
-        caps.setCapability("app", System.getProperty("user.dir") + "/apps/Todo.apk");
+        caps.setCapability("appPackage", "com.google.android.dialer");
+        caps.setCapability("appActivity", "com.google.android.dialer.extensions.GoogleDialtactsActivity");
         driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
     }
-
     @Test
-    public void click_App_Button() {
-        // code
-        driver.findElementByAccessibilityId("App").click();
+    public void click_test() {
+        driver.findElement(By.id("dialpad_fab")).click();
+        driver.findElement(By.id("zero")).click();
+        driver.findElement(By.id("one")).click();
+        driver.findElement(By.id("two")).click();
     }
 
     @AfterTest
@@ -35,5 +38,4 @@ public class FirstAndroidTest {
             driver.quit();
         }
     }
-
 }
